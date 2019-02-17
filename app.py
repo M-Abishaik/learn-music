a77fa7e63216dde6540acd8574e9f2a8c084bbb3
santhoshkumar <santhoshkumar.ssk3222@gmail.com>
2019-02-18 01:04:14 +0530

    initial phase

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ lessons={
 	"lesson1":["s", "r", "g", "m"],
 	"lesson2":["s", "r", "g", "m","p","d","n","s"]
 }
-base_addr="http://127.0.0.1:5000"
-# base_addr="https://playmusicapp.herokuapp.com"
+# base_addr="http://127.0.0.1:5000"
+base_addr="https://playmusicapp.herokuapp.com"
 # base_addr="http://192.168.43.125:5000"
 app=Flask('__name__')
 @app.route('/')
